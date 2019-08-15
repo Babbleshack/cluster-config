b3eef776547bafad9e788ac270019ad94d1118bd
babbleshack <dcrl94@gmail.com>
2019-08-15 16:45:57 +0100

    Sensible config, debugged
    This config contains sensible kubernetes.
    WORKS

---
+++ helper-scripts/delete-namespace.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
+curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
